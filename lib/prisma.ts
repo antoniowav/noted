21b5b106adffd4aab2e6ed8c05970b60e55326e4
Antonio Piattelli <antonio.piattelli@zimpler.com>
2025-01-21 13:16:00 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,9 @@
-import { PrismaClient as Client } from ".prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new Client();
-export type PrismaClient = Client;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
