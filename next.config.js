72db23f052c3444053eb29a64e21168b97d641be
Antonio Piattelli <antonio.piattelli@zimpler.com>
2025-01-21 13:00:03 +0100

    build fix 10

--- next.config.js
+++ next.config.js
@@ -13,7 +13,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: "standalone",
   experimental: {
     serverActions: {
       allowedOrigins: ["takenoted.netlify.app"],
