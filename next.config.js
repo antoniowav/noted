21920712f4227cdc7f81feeea16ffa9919b76dac
Antonio Piattelli <antonio.piattelli@zimpler.com>
2025-01-21 14:59:24 +0100

    build issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
